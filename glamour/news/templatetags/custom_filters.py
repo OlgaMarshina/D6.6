5e52b534256066deae11a43f5e3c18037c2dc8f6
OlgaMarshina <127740474+OlgaMarshina@users.noreply.github.com>
2023-11-17 09:27:26 +0300

    Add files via upload

---
+++ glamour/news/templatetags/custom_filters.py
@@ -0,0 +1,18 @@
+from django import template
+
+
+register = template.Library()
+
+cens = ['мех', 'кровь', 'бритье', 'стресс']
+
+
+@register.filter()
+def censor(word):
+
+ if isinstance(word, str):
+      for i in cens:
+         word = word.replace(i[1:], '*' * len(i[1:]))
+      else:
+         raise ValueError(
+         'custom_filters -> censor -> A string is expected, but a different data type has been entered')
+      return word
